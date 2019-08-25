556e230e4eafe8aaae57829634054e13d27733ca
Zineng Tang <37575766+dowhilefalse@users.noreply.github.com>
2019-08-25 14:22:18 -0400

    Update A1Novice.java

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -3,87 +3,34 @@ package a1;
 import java.util.Scanner;
 
 public class A1Novice {
+
 	public static void main(String[] args) {
-
+		
 		Scanner scan = new Scanner(System.in);
-
-		// Read in count of numbers to process
-		
-		int count = scan.nextInt();
-		
-		
-		// Create an array to store numbers.
-		
-		int[] values = new int[count];
-		
-		
-		// Read values into the array
-		
-		for (int i=0; i<values.length; i++) {
-			values[i] = scan.nextInt();
-		}
-		
-		// All input parsed, so close scanner
-		scan.close();
-		
-		// Use helper functions to calculate sum, min, and max
-		
-		int sum = calculateValueSum(values);
-		int min = findValueMin(values);
-		int max = findValueMax(values);
-		
-		// Print results
-		
-		System.out.println("Min: " + min);
-		System.out.println("Max: " + max);
-		System.out.println("Average: " + String.format("%.2f", ((double) sum) / ((double) count)));
-		
-	}
-
-	
-	static int calculateValueSum(int[] vals) {
-		
-		int sum = 0;
-		
-		for (int i=0; i<vals.length; i++) {
-			sum += vals[i];
-		}
-		
-		return sum;
-	}
-
-	static int findValueMin(int[] vals) {
-		
-		// Initialize current minimum to first value in array.
-		int cur_min = vals[0];
-		
-		// Starting with second value (if any), compare each value
-		// in array with current minimum and replace if smaller.
-		
-		for (int i=1; i < vals.length; i++) {
-			if (vals[i] < cur_min) {
-				cur_min = vals[i];
+		int num_customers = scan.nextInt();
+		String [] customer_names = new String[num_customers];
+	    double [] total_prices = new double[num_customers];
+		for (int i=0; i<num_customers; i++) {
+			String customer_first_name = scan.next();
+			String customer_last_name = scan.next();
+			String customer_name = customer_first_name.substring(0,1)+". "+customer_last_name;
+			customer_names[i]=customer_name;
+			int num_fruits = scan.nextInt();
+//			System.out.println(num_fruits);
+			double total_price = 0;
+			for (int j=0; j<num_fruits; j++) {
+				int quantity = scan.nextInt();
+				String name = scan.next();
+				double price = scan.nextDouble();
+				total_price += quantity*price;
 			}
+			total_prices[i]=total_price;
+		}
+		for (int i=0; i<num_customers; i++) {
+			System.out.println(customer_names[i]+": "+total_prices[i]);
 		}
 		
-		return cur_min;
-	}
-
-
-	static int findValueMax(int[] vals) {
+		// Your code follows here.
 		
-		// Initialize current minimum to first value in array.
-		int cur_max = vals[0];
-		
-		// Starting with second value (if any), compare each value
-		// in array with current minimum and replace if smaller.
-		
-		for (int i=1; i < vals.length; i++) {
-			if (vals[i] > cur_max) {
-				cur_max = vals[i];
-			}
-		}
-		
-		return cur_max;
 	}
 }
